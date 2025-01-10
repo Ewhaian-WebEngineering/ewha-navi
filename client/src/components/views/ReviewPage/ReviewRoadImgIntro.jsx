1c2b7be47e5c08dd9c552f6a8e8b9787ca9eb636
Hee Seo Kim <hisounni@ewhain.net>
2025-01-11 03:11:25 +0900

    이미지 추가

--- client/src/components/views/ReviewPage/ReviewRoadImgIntro.jsx
+++ client/src/components/views/ReviewPage/ReviewRoadImgIntro.jsx
@@ -1,46 +1,84 @@
 import React from "react";
 import styled from "styled-components";
 import SmallStarIcon from "../../utils/icons/SmallStarIcon.svg";
+import mainGate from "../../images/shortcuts/mainGate.png";
+import backGate from "../../images/shortcuts/backGate.png";
+import poscoAsan from "../../images/shortcuts/poscoAsan.png";
+import business from "../../images/shortcuts/business.png";
+import asan from "../../images/shortcuts/asan.png";
+import asanBusiness from "../../images/shortcuts/asanBusiness.png";
+import podo from "../../images/shortcuts/podo.png";
+import hakEcc from "../../images/shortcuts/hakEcc.png";
+
+const imageMapping = {
+    "정문-기숙사": mainGate,
+    "후문-기숙사": backGate,
+    "정문-학생문화관":mainGate,
+    "포스코관-아산공학관":poscoAsan,
+    "신세계관-중앙도서관":business,
+    "아산공학관-법학관":asan,
+    "아산공학관-신세계관":asanBusiness,
+    "포도길":podo,
+    "학관-ECC":hakEcc,
+    "신세계관-ECC":business,
+    
+    "신세계관-포스코관":mainGate,
+    "ECC-생활환경관":mainGate,
+    "포스코관-학관":mainGate,
+    "포스코관-종합과학관":mainGate,
+    "아산공학관-연구협력관":mainGate,
+    "종합과학관-음악관":mainGate,
+    "기숙사-음악관":mainGate,
+    "신세계관-생활환경관":mainGate,
+    "아산공학관-학관":mainGate,
+    "학관-아산공학관":mainGate,
+    "체육관-학관":mainGate,
+    "학관-체육관":mainGate,
+    "ECC-중앙도서관":mainGate,
+    "중앙도서관-ECC":mainGate,
+
+    "포스코관": "POSCO.png",
+    "신세계관": "newHall.png",
+    "아산 공학관": "AsanEngineering.png",
+    "ECC": "ECC.png",
+    "종합과학관": "jongScience.png",
+    "기숙사": "Dormitory.png",
+    "학관": "hakgwan.png",
+    "체육관": "gym.png",
+    "중앙 도서관": "centralLibrary.png",
+  };
 
 
-const ReviewRoadImageIntro = (props)=>{
-    return<>
-    <GradationBox>
-        <RoadInfoBox>
-            {/* 이미지 렌더링 추가 */}
-          {/* <img 
-          src={props.imageSrc || "/images/shortcuts/defaultImage.png"} 
-          alt={`${props.roadName} 이미지`} 
-          style={{
-            width: "100px", // 이미지가 가로로 꽉 차도록 설정
-            height: "100px", // 비율 유지
-            objectFit: "cover", 
-            borderRadius: "10px", // 둥근 모서리 (선택사항)
-            marginBottom: "10px" // 간격
-          }}
-          /> */}
 
-
-
-
-
-
-
-
-
-
-            <RoadTitle>{props.roadName}</RoadTitle>
-            <RoadStartEnd>({props.start}) -  ({props.end})</RoadStartEnd>
-        </RoadInfoBox>
-        <RatingBox>
-            <div><img src={SmallStarIcon} alt = "smallstar"/></div>
-            <RatingNum>{props.rating}</RatingNum>
-        </RatingBox>
-    </GradationBox>
-    </>
+const ReviewRoadImageIntro = (props) => {
+    return (
+        <>
+            <GradationBox image={imageMapping[props.roadName] || mainGate}>
+                <RoadInfoBox>
+                    <RoadTitle>{props.roadName}</RoadTitle>
+                    <RoadStartEnd>({props.start}) -  ({props.end})</RoadStartEnd>
+                </RoadInfoBox>
+                <RatingBox>
+                    <div><img src={SmallStarIcon} alt="smallstar" /></div>
+                    <RatingNum>{props.rating}</RatingNum>
+                </RatingBox>
+            </GradationBox>
+        </>
+    );
 }
 
+const FullImage = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+`;
+
 const GradationBox = styled.div`
+    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: flex-end;
@@ -51,7 +89,9 @@ const GradationBox = styled.div`
     max-width: 390px;
     margin-left: auto;
     margin-right: auto;
-    background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 100%), #D9D9D9;
+    background: ${({ image }) => `url(${image}) no-repeat center center`};
+    background-size: cover;
+    background-blend-mode: overlay;
 `;
 
 const RoadInfoBox = styled.div`
